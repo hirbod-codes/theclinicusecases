8ecf932322fb042904bcb8c041aa21b4c8803fef
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-21 00:23:08 -0800

    Update AccountsManagement::getAccounts method
    Added a method to check authentication and authorization and accountsRead privilege.

--- Src/Accounts/AccountsManagement.php
+++ Src/Accounts/AccountsManagement.php
@@ -2,15 +2,37 @@
 
 namespace TheClinicUseCases\Accounts;
 
+use TheClinic\DataStructures\User\DSUser;
 use TheClinicUseCases\Accounts\Interfaces\IDataBaseRetrieveAccounts;
+use TheClinicUseCases\Exceptions\PrivilegeNotFound;
+use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthenticated;
+use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthorized;
 
 class AccountsManagement
 {
     /**
      * @return \TheClinic\DataStructures\User\DSUser[]
      */
-    public function getAccounts(int $lastVisitId, int $count, IDataBaseRetrieveAccounts $db): array
+    public function getAccounts(int $lastVisitId, int $count, DSUser $user, IDataBaseRetrieveAccounts $db): array
     {
+        $this->checkAccountsReadPrivilege($user);
+
         return $db->getAccounts($lastVisitId, $count);
+    }
+
+    public function checkAccountsReadPrivilege(DSUser $user): void
+    {
+        if (!$user->isAuthenticated()) {
+            throw new UserIsNotAuthenticated();
+        }
+
+        $role = $user->getRole();
+
+        if ($role->privilegeExists("accountsRead") && $role->getPrivilegeValue("accountsRead") === true) {
+        } elseif (!$role->privilegeExists("accountsRead")) {
+            throw new PrivilegeNotFound();
+        } else {
+            throw new UserIsNotAuthorized();
+        }
     }
 }
