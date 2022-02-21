ac627b5a84d29f2d766f9ba4e3ab947b611f1b55
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-21 01:33:49 -0800

    Update AccountsManagementTest test class
    Added a test for AccountsManagement::updateAccount method.

--- Tests/Accounts/AccountsManagementTest.php
+++ Tests/Accounts/AccountsManagementTest.php
@@ -12,6 +12,7 @@ use TheClinicUseCases\Accounts\AccountsManagement;
 use TheClinicUseCases\Accounts\Interfaces\IDataBaseCreateAccount;
 use TheClinicUseCases\Accounts\Interfaces\IDataBaseDeleteAccount;
 use TheClinicUseCases\Accounts\Interfaces\IDataBaseRetrieveAccounts;
+use TheClinicUseCases\Accounts\Interfaces\IDataBaseUpdateAccount;
 use TheClinicUseCases\Exceptions\PrivilegeNotFound;
 use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthenticated;
 use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthorized;
@@ -235,6 +236,76 @@ class AccountsManagementTest extends TestCase
             $user->shouldReceive("getRole")->andReturn($rule);
 
             (new AccountsManagement)->deleteAccount($targetUser, $user, $db);
+            throw new \RuntimeException("Failure!!!", 500);
+        } catch (UserIsNotAuthenticated $th) {
+            $this->assertEquals("The current authenticated user is not authenticated.", $th->getMessage());
+        }
+    }
+
+    public function testUpdateAccount(): void
+    {
+        /** @var \TheClinicUseCases\Accounts\Interfaces\IDataBaseUpdateAccount|\Mockery\MockInterface $db */
+        $db = Mockery::mock(IDataBaseUpdateAccount::class);
+        $db->shouldReceive("updateAccount")->with([]);
+
+        /** @var \TheClinic\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
+        $rule = Mockery::mock(IUserRule::class);
+        $rule->shouldReceive("privilegeExists")->with("accountUpdate")->andReturn(true);
+        $rule->shouldReceive("getPrivilegeValue")->with("accountUpdate")->andReturn(true);
+
+        /** @var \TheClinic\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+        $user = Mockery::mock(DSUser::class);
+        $user->shouldReceive("isAuthenticated")->andReturn(true);
+        $user->shouldReceive("getRole")->andReturn($rule);
+
+        (new AccountsManagement)->updateAccount([], $user, $db);
+
+        try {
+            /** @var \TheClinic\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
+            $rule = Mockery::mock(IUserRule::class);
+            $rule->shouldReceive("privilegeExists")->with("accountUpdate")->andReturn(false);
+            $rule->shouldReceive("getPrivilegeValue")->with("accountUpdate")->andReturn(true);
+
+            /** @var \TheClinic\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+            $user = Mockery::mock(DSUser::class);
+            $user->shouldReceive("isAuthenticated")->andReturn(true);
+            $user->shouldReceive("getRole")->andReturn($rule);
+
+            (new AccountsManagement)->updateAccount([], $user, $db);
+            throw new \RuntimeException("Failure!!!", 500);
+        } catch (PrivilegeNotFound $th) {
+            $this->assertEquals("There is no such privilege.", $th->getMessage());
+        }
+
+        try {
+            /** @var \TheClinic\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
+            $rule = Mockery::mock(IUserRule::class);
+            $rule->shouldReceive("privilegeExists")->with("accountUpdate")->andReturn(true);
+            $rule->shouldReceive("getPrivilegeValue")->with("accountUpdate")->andReturn(false);
+
+            /** @var \TheClinic\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+            $user = Mockery::mock(DSUser::class);
+            $user->shouldReceive("isAuthenticated")->andReturn(true);
+            $user->shouldReceive("getRole")->andReturn($rule);
+
+            (new AccountsManagement)->updateAccount([], $user, $db);
+            throw new \RuntimeException("Failure!!!", 500);
+        } catch (UserIsNotAuthorized $th) {
+            $this->assertEquals("The current authenticated user is not authorized for this action.", $th->getMessage());
+        }
+
+        try {
+            /** @var \TheClinic\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
+            $rule = Mockery::mock(IUserRule::class);
+            $rule->shouldReceive("privilegeExists")->with("accountUpdate")->andReturn(true);
+            $rule->shouldReceive("getPrivilegeValue")->with("accountUpdate")->andReturn(true);
+
+            /** @var \TheClinic\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+            $user = Mockery::mock(DSUser::class);
+            $user->shouldReceive("isAuthenticated")->andReturn(false);
+            $user->shouldReceive("getRole")->andReturn($rule);
+
+            (new AccountsManagement)->updateAccount([], $user, $db);
             throw new \RuntimeException("Failure!!!", 500);
         } catch (UserIsNotAuthenticated $th) {
             $this->assertEquals("The current authenticated user is not authenticated.", $th->getMessage());
