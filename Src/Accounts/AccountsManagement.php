0fc5ac9dad58d28a8cc7174d2a2d11d7340aff1a
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-21 01:05:36 -0800

    Update AccountsManagement class
    Added a method for creating a new account, and
    a method to check user's authentication and authorization for account creation.

--- Src/Accounts/AccountsManagement.php
+++ Src/Accounts/AccountsManagement.php
@@ -3,6 +3,7 @@
 namespace TheClinicUseCases\Accounts;
 
 use TheClinic\DataStructures\User\DSUser;
+use TheClinicUseCases\Accounts\Interfaces\IDataBaseCreateAccount;
 use TheClinicUseCases\Accounts\Interfaces\IDataBaseRetrieveAccounts;
 use TheClinicUseCases\Exceptions\PrivilegeNotFound;
 use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthenticated;
@@ -30,6 +31,29 @@ class AccountsManagement
 
         if ($role->privilegeExists("accountsRead") && $role->getPrivilegeValue("accountsRead") === true) {
         } elseif (!$role->privilegeExists("accountsRead")) {
+            throw new PrivilegeNotFound();
+        } else {
+            throw new UserIsNotAuthorized();
+        }
+    }
+
+    public function createAccount(array $input, DSUser $user, IDataBaseCreateAccount $db)
+    {
+        $this->checkAccountsCreatePrivilege($user);
+
+        $db->createAccount($input);
+    }
+
+    private function checkAccountsCreatePrivilege(DSUser $user): void
+    {
+        if (!$user->isAuthenticated()) {
+            throw new UserIsNotAuthenticated();
+        }
+
+        $role = $user->getRole();
+
+        if ($role->privilegeExists("accountsCreate") && $role->getPrivilegeValue("accountsCreate") === true) {
+        } elseif (!$role->privilegeExists("accountsCreate")) {
             throw new PrivilegeNotFound();
         } else {
             throw new UserIsNotAuthorized();
