3fba6d02c346cc31c748567a8fe289d847560b00
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-24 01:44:31 -0800

    Update OrderManagement class
    Moved the authentication and authorization check to separate classes and
    Removed two private methods that were previously responsible for that.

--- Src/Orders/OrderManagement.php
+++ Src/Orders/OrderManagement.php
@@ -5,57 +5,38 @@ namespace TheClinicUseCases\Orders;
 use TheClinic\DataStructures\Order\DSOrders;
 use TheClinic\DataStructures\Order\DSLaserOrders;
 use TheClinic\DataStructures\User\DSUser;
-use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthenticated;
-use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthorized;
-use TheClinicUseCases\Exceptions\PrivilegeNotFound;
+use TheClinicUseCases\Accounts\Authentication;
 use TheClinicUseCases\Orders\Interfaces\IDataBaseRetrieveLaserOrders;
 use TheClinicUseCases\Orders\Interfaces\IDataBaseRetrieveOrders;
+use TheClinicUseCases\Privileges\PrivilegesManagement;
 
 class OrderManagement
 {
+    private Authentication $authentication;
+
+    private PrivilegesManagement $privilegesManagement;
+
+    public function __construct(
+        Authentication $authentication,
+        PrivilegesManagement $privilegesManagement
+    ) {
+        $this->authentication = $authentication;
+        $this->privilegesManagement = $privilegesManagement;
+    }
+
     public function getOrders(DSUser $user, int $lastOrderId = null, int $count, IDataBaseRetrieveOrders $db): DSOrders
     {
-        $this->checkOrdersReadPrivilege($user);
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "regularOrdersRead");
 
         return $db->getOrders($lastOrderId, $count);
     }
 
-    private function checkOrdersReadPrivilege(DSUser $user): void
-    {
-        if (!$user->isAuthenticated()) {
-            throw new UserIsNotAuthenticated();
-        }
-
-        $role = $user->getRole();
-
-        if ($role->privilegeExists("ordersRead") && $role->getPrivilegeValue("ordersRead") === true) {
-        } elseif (!$role->privilegeExists("ordersRead")) {
-            throw new PrivilegeNotFound();
-        } else {
-            throw new UserIsNotAuthorized();
-        }
-    }
-
     public function getLaserOrders(DSUser $user, int $lastLaserOrderId = null, int $count, IDataBaseRetrieveLaserOrders $db): DSLaserOrders
     {
-        $this->checkLaserOrdersReadPrivilege($user);
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "laserOrdersRead");
 
         return $db->getLaserOrders($lastLaserOrderId, $count);
-    }
-
-    private function checkLaserOrdersReadPrivilege(DSUser $user): void
-    {
-        if (!$user->isAuthenticated()) {
-            throw new UserIsNotAuthenticated();
-        }
-
-        $role = $user->getRole();
-
-        if ($role->privilegeExists("laserOrdersRead") && $role->getPrivilegeValue("laserOrdersRead") === true) {
-        } elseif (!$role->privilegeExists("laserOrdersRead")) {
-            throw new PrivilegeNotFound();
-        } else {
-            throw new UserIsNotAuthorized();
-        }
     }
 }
