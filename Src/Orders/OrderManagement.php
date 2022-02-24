7f9d17ca42a37a5e85ca483b06954d776eb5770b
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-24 01:50:02 -0800

    Update OrderManagement class
    Added a method for creaating a laser order method.

--- Src/Orders/OrderManagement.php
+++ Src/Orders/OrderManagement.php
@@ -2,13 +2,25 @@
 
 namespace TheClinicUseCases\Orders;
 
+use TheClinic\DataStructures\Order\DSLaserOrder;
 use TheClinic\DataStructures\Order\DSOrders;
 use TheClinic\DataStructures\Order\DSLaserOrders;
 use TheClinic\DataStructures\Order\DSOrder;
+use TheClinic\DataStructures\Order\DSPackages;
+use TheClinic\DataStructures\Order\DSParts;
 use TheClinic\DataStructures\User\DSUser;
+use TheClinic\Exceptions\Order\InvalidGenderException;
+use TheClinic\Exceptions\Order\NoPackageOrPartException;
+use TheClinic\Order\ICalculateLaserOrder;
 use TheClinic\Order\ICalculateRegularOrder;
+use TheClinic\Order\Laser\Calculations\PriceCalculator;
+use TheClinic\Order\Laser\Calculations\TimeConsumptionCalculator;
+use TheClinic\Order\Laser\ILaserPriceCalculator;
+use TheClinic\Order\Laser\ILaserTimeConsumptionCalculator;
+use TheClinic\Order\Laser\LaserOrder;
 use TheClinic\Order\Regular\RegularOrder;
 use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Orders\Interfaces\IDataBaseCreateLaserOrder;
 use TheClinicUseCases\Orders\Interfaces\IDataBaseCreateRegularOrder;
 use TheClinicUseCases\Orders\Interfaces\IDataBaseRetrieveLaserOrders;
 use TheClinicUseCases\Orders\Interfaces\IDataBaseRetrieveOrders;
@@ -22,13 +34,26 @@ class OrderManagement
 
     private ICalculateRegularOrder $iCalculateRegularOrder;
 
+    private ICalculateLaserOrder $iCalculateLaserOrder;
+
+    private ILaserPriceCalculator $iLaserPriceCalculator;
+
+    private ILaserTimeConsumptionCalculator $iLaserTimeConsumptionCalculator;
+
     public function __construct(
         Authentication $authentication,
         PrivilegesManagement $privilegesManagement,
-        ICalculateRegularOrder $iCalculateRegularOrder = null
+        ICalculateRegularOrder $iCalculateRegularOrder = null,
+        ICalculateLaserOrder $iCalculateLaserOrder = null,
+        ILaserPriceCalculator $iLaserPriceCalculator = null,
+        ILaserTimeConsumptionCalculator $iLaserTimeConsumptionCalculator = null
     ) {
         $this->iCalculateRegularOrder = $iCalculateRegularOrder ?: new RegularOrder;
-        
+
+        $this->iLaserPriceCalculator = $iLaserPriceCalculator ?: new PriceCalculator;
+        $this->iLaserTimeConsumptionCalculator = $iLaserTimeConsumptionCalculator ?: new TimeConsumptionCalculator;
+        $this->iCalculateLaserOrder = $iCalculateLaserOrder ?: new LaserOrder;
+
         $this->authentication = $authentication;
         $this->privilegesManagement = $privilegesManagement;
     }
@@ -55,5 +80,26 @@ class OrderManagement
         $this->privilegesManagement->checkBool($user, "regularOrderCreate");
 
         return $db->createRegularOrder($user, $this->iCalculateRegularOrder->calculatePrice(), $this->iCalculateRegularOrder->calculateTimeConsumption());
+    }
+
+    public function createLaserOrder(DSUser $user, DSUser $targetUser, ?DSParts $parts = null, ?DSPackages $packages = null, IDataBaseCreateLaserOrder $db): DSLaserOrder
+    {
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "laserOrderCreate");
+
+        if (($parts !== null && $targetUser->getGender() !== $parts->getGender()) || ($packages !== null && $targetUser->getGender() !== $packages->getGender())) {
+            throw new InvalidGenderException("User, parts and packages must have the same gender.", 500);
+        } elseif ($parts === null && $packages === null) {
+            throw new NoPackageOrPartException("One of the parts or packages must exist.", 500);
+        }
+
+        return $db->createLaserOrder(
+            $targetUser,
+            $parts,
+            $packages,
+            $this->iCalculateLaserOrder->calculatePrice($parts, $packages, $this->iLaserPriceCalculator),
+            $this->iCalculateLaserOrder->calculateTimeConsumption($parts, $packages, $this->iLaserTimeConsumptionCalculator),
+            $this->iCalculateLaserOrder->calculatePriceWithoutDiscount($parts, $packages, $this->iLaserPriceCalculator)
+        );
     }
 }
