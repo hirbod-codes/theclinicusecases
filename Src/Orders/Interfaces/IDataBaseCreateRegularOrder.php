82bdc216cd5f99bf8c9c34d87f551ed4d331b4d8
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 03:52:31 -0800

    Fix the import of DSRegularOrder class in IDataBaseCreateRegularOrder

--- Src/Orders/Interfaces/IDataBaseCreateRegularOrder.php
+++ Src/Orders/Interfaces/IDataBaseCreateRegularOrder.php
@@ -2,10 +2,10 @@
 
 namespace TheClinicUseCases\Orders\Interfaces;
 
-use TheClinicDataStructures\DataStructures\Order\DSOrder;
+use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrder;
 use TheClinicDataStructures\DataStructures\User\DSUser;
 
 interface IDataBaseCreateRegularOrder
 {
-    public function createRegularOrder(DSUser $user, int $price, int $timeConsumption): DSOrder;
+    public function createRegularOrder(DSUser $user, int $price, int $timeConsumption): DSRegularOrder;
 }
