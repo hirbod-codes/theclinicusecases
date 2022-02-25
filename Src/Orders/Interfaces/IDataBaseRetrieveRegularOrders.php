bd8d486a49d7c3201d2877dbfe40b3d261965a01
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 03:55:13 -0800

    Add getSelfRegularOrders method IDataBaseRetrieveRegularOrders interface
    Added a method for getting user's own regular orders in IDataBaseRetrieveRegularOrders interface.

--- Src/Orders/Interfaces/IDataBaseRetrieveRegularOrders.php
+++ Src/Orders/Interfaces/IDataBaseRetrieveRegularOrders.php
@@ -3,8 +3,11 @@
 namespace TheClinicUseCases\Orders\Interfaces;
 
 use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrders;
+use TheClinicDataStructures\DataStructures\User\DSUser;
 
 interface IDataBaseRetrieveRegularOrders
 {
     public function getRegularOrders(int $lastOrderId = null, int $count): DSRegularOrders;
+
+    public function getSelfRegularOrders(DSUser $user): DSRegularOrders;
 }
