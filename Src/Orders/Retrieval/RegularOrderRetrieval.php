1d69f9c76b4dfeeacbe2db19464d758dc59aceb4
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-26 00:40:22 -0800

    Add methods to RegularOrderRetrieval for IDataBaseRetrieveRegularOrders

--- Src/Orders/Retrieval/RegularOrderRetrieval.php
+++ Src/Orders/Retrieval/RegularOrderRetrieval.php
@@ -2,20 +2,116 @@
 
 namespace TheClinicUseCases\Orders\Retrieval;
 
+use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrder;
+use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrders;
+use TheClinicDataStructures\DataStructures\User\DSUser;
 use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Orders\Interfaces\IDataBaseRetrieveRegularOrders;
 use TheClinicUseCases\Privileges\PrivilegesManagement;
 
-class OrderRetrieval
+class RegularOrderRetrieval
 {
+    private static array $operatorValues = ["<=", ">=", "=", "<>", "<", ">"];
+
     private Authentication $authentication;
 
     private PrivilegesManagement $privilegesManagement;
 
     public function __construct(
-        Authentication $authentication,
-        PrivilegesManagement $privilegesManagement,
+        Authentication $authentication = null,
+        PrivilegesManagement $privilegesManagement = null,
     ) {
-        $this->authentication = $authentication;
-        $this->privilegesManagement = $privilegesManagement;
+        $this->authentication = $authentication ?: new Authentication;
+        $this->privilegesManagement = $privilegesManagement ?: new PrivilegesManagement;
+    }
+
+    public function getRegularOrdersByPriceByUser(string $operator, int $price, DSUser $targetUser, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrders
+    {
+        if (!in_array($operator, self::$operatorValues)) {
+            throw new \RuntimeException("The operator parameter has an invalid value.", 500);
+        }
+
+        if ($targetUser->getId() === $user->getId()) {
+            $privilege = "selfRegularOrdersRead";
+        } else {
+            $privilege = "regularOrdersRead";
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, $privilege);
+
+        return $db->getRegularOrdersByPriceByUser($operator, $price, $targetUser);
+    }
+
+    public function getRegularOrdersByPrice(int $lastOrderId = null, int $count, string $operator, int $price, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrders
+    {
+        if (!in_array($operator, self::$operatorValues)) {
+            throw new \RuntimeException("The operator parameter has an invalid value.", 500);
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "regularOrdersRead");
+
+        return $db->getRegularOrdersByPrice($lastOrderId, $count, $operator, $price);
+    }
+
+    public function getRegularOrdersByTimeConsumptionByUser(string $operator, int $timeCosumption, DSUser $targetUser, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrders
+    {
+        if (!in_array($operator, self::$operatorValues)) {
+            throw new \RuntimeException("The operator parameter has an invalid value.", 500);
+        }
+
+        if ($targetUser->getId() === $user->getId()) {
+            $privilege = "selfRegularOrdersRead";
+        } else {
+            $privilege = "regularOrdersRead";
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, $privilege);
+
+        return $db->getRegularOrdersByTimeConsumptionByUser($operator, $timeCosumption, $targetUser);
+    }
+
+    public function getRegularOrdersByTimeConsumption(int $lastOrderId = null, int $count, string $operator, int $timeCosumption, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrders
+    {
+        if (!in_array($operator, self::$operatorValues)) {
+            throw new \RuntimeException("The operator parameter has an invalid value.", 500);
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "regularOrdersRead");
+
+        return $db->getRegularOrdersByTimeConsumption($lastOrderId, $count, $operator, $timeCosumption);
+    }
+
+    public function getRegularOrdersByUser(DSUser $targetUser, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrders
+    {
+        if ($targetUser->getId() === $user->getId()) {
+            $privilege = "selfRegularOrdersRead";
+        } else {
+            $privilege = "regularOrdersRead";
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, $privilege);
+
+        return $db->getRegularOrdersByUser($targetUser);
+    }
+
+    public function getRegularOrders(int $lastOrderId = null, int $count, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrders
+    {
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "regularOrdersRead");
+
+        return $db->getRegularOrders($lastOrderId, $count);
+    }
+
+    public function getRegularOrderById(int $id, DSUser $user, IDataBaseRetrieveRegularOrders $db): DSRegularOrder
+    {
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, "regularOrdersRead");
+
+        return $db->getRegularOrderById($id);
     }
 }
