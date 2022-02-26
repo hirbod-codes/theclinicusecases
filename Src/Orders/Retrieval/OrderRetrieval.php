95699ee1776340572b3b5d786283b8de0f30a930
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 22:52:02 -0800

    Add __construct method and it's properties in OrderRetrieval class

--- Src/Orders/Retrieval/OrderRetrieval.php
+++ Src/Orders/Retrieval/OrderRetrieval.php
@@ -2,6 +2,20 @@
 
 namespace TheClinicUseCases\Orders\Retrieval;
 
+use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Privileges\PrivilegesManagement;
+
 class OrderRetrieval
 {
+    private Authentication $authentication;
+
+    private PrivilegesManagement $privilegesManagement;
+
+    public function __construct(
+        Authentication $authentication,
+        PrivilegesManagement $privilegesManagement,
+    ) {
+        $this->authentication = $authentication;
+        $this->privilegesManagement = $privilegesManagement;
+    }
 }
