c04cd1676c49e73dc977947e93a7195d4de8ebf6
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 22:40:27 -0800

    Add a method to create a laser order in OrderCreation class

--- Src/Orders/Creation/OrderCreation.php
+++ Src/Orders/Creation/OrderCreation.php
@@ -2,6 +2,8 @@
 
 namespace TheClinicUseCases\Orders\Creation;
 
+use TheClinic\Exceptions\Order\InvalidGenderException;
+use TheClinic\Exceptions\Order\NoPackageOrPartException;
 use TheClinic\Order\ICalculateLaserOrder;
 use TheClinic\Order\ICalculateRegularOrder;
 use TheClinic\Order\Laser\Calculations\PriceCalculator;
@@ -10,9 +12,13 @@ use TheClinic\Order\Laser\ILaserPriceCalculator;
 use TheClinic\Order\Laser\ILaserTimeConsumptionCalculator;
 use TheClinic\Order\Laser\LaserOrder;
 use TheClinic\Order\Regular\RegularOrder;
+use TheClinicDataStructures\DataStructures\Order\DSPackages;
+use TheClinicDataStructures\DataStructures\Order\DSParts;
+use TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrder;
 use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrder;
 use TheClinicDataStructures\DataStructures\User\DSUser;
 use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Orders\Interfaces\IDataBaseCreateLaserOrder;
 use TheClinicUseCases\Orders\Interfaces\IDataBaseCreateRegularOrder;
 use TheClinicUseCases\Privileges\PrivilegesManagement;
 
@@ -60,5 +66,32 @@ class OrderCreation
         $this->privilegesManagement->checkBool($user, $privilege);
 
         return $db->createRegularOrder($targetUser, $this->iCalculateRegularOrder->calculatePrice(), $this->iCalculateRegularOrder->calculateTimeConsumption());
+    }
+
+    public function createLaserOrder(DSUser $targetUser, DSUser $user, ?DSParts $parts = null, ?DSPackages $packages = null, IDataBaseCreateLaserOrder $db): DSLaserOrder
+    {
+        if ($targetUser->getId() === $user->getId()) {
+            $privilege = "selfLaserOrderCreate";
+        } else {
+            $privilege = "laserOrderCreate";
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, $privilege);
+
+        if (($parts !== null && $targetUser->getGender() !== $parts->getGender()) || ($packages !== null && $targetUser->getGender() !== $packages->getGender())) {
+            throw new InvalidGenderException("User, parts and packages must have the same gender.", 500);
+        } elseif ($parts === null && $packages === null) {
+            throw new NoPackageOrPartException("One of the parts or packages must exist.", 500);
+        }
+
+        return $db->createLaserOrder(
+            $targetUser,
+            $parts,
+            $packages,
+            $this->iCalculateLaserOrder->calculatePrice($parts, $packages, $this->iLaserPriceCalculator),
+            $this->iCalculateLaserOrder->calculateTimeConsumption($parts, $packages, $this->iLaserTimeConsumptionCalculator),
+            $this->iCalculateLaserOrder->calculatePriceWithoutDiscount($parts, $packages, $this->iLaserPriceCalculator)
+        );
     }
 }
