b1b4527e2d1809ece80fa6ef1336ed89fc7a6db1
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-26 02:15:59 -0800

    Create IDataBaseDeleteRegularOrder interface

---
+++ Src/Orders/Interfaces/IDataBaseDeleteRegularOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TheClinicUseCases\Orders\Interfaces;
+
+use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrder;
+use TheClinicDataStructures\DataStructures\User\DSUser;
+
+interface IDataBaseDeleteRegularOrder
+{
+    public function deleteRegularOrder(DSRegularOrder $regularOrder, DSUser $user): void;
+}
