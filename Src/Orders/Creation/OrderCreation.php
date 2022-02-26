87d38ae6fd11084af8995a11ed14e7e648d8876c
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 21:33:26 -0800

    Add __construct method and it's properties in OrderCreation class

--- Src/Orders/Creation/OrderCreation.php
+++ Src/Orders/Creation/OrderCreation.php
@@ -2,6 +2,46 @@
 
 namespace TheClinicUseCases\Orders\Creation;
 
+use TheClinic\Order\ICalculateLaserOrder;
+use TheClinic\Order\ICalculateRegularOrder;
+use TheClinic\Order\Laser\Calculations\PriceCalculator;
+use TheClinic\Order\Laser\Calculations\TimeConsumptionCalculator;
+use TheClinic\Order\Laser\ILaserPriceCalculator;
+use TheClinic\Order\Laser\ILaserTimeConsumptionCalculator;
+use TheClinic\Order\Laser\LaserOrder;
+use TheClinic\Order\Regular\RegularOrder;
+use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Privileges\PrivilegesManagement;
+
 class OrderCreation
 {
+    private Authentication $authentication;
+
+    private PrivilegesManagement $privilegesManagement;
+
+    private ICalculateRegularOrder $iCalculateRegularOrder;
+
+    private ICalculateLaserOrder $iCalculateLaserOrder;
+
+    private ILaserPriceCalculator $iLaserPriceCalculator;
+
+    private ILaserTimeConsumptionCalculator $iLaserTimeConsumptionCalculator;
+
+    public function __construct(
+        Authentication $authentication,
+        PrivilegesManagement $privilegesManagement,
+        ICalculateRegularOrder $iCalculateRegularOrder = null,
+        ICalculateLaserOrder $iCalculateLaserOrder = null,
+        ILaserPriceCalculator $iLaserPriceCalculator = null,
+        ILaserTimeConsumptionCalculator $iLaserTimeConsumptionCalculator = null
+    ) {
+        $this->iCalculateRegularOrder = $iCalculateRegularOrder ?: new RegularOrder;
+
+        $this->iLaserPriceCalculator = $iLaserPriceCalculator ?: new PriceCalculator;
+        $this->iLaserTimeConsumptionCalculator = $iLaserTimeConsumptionCalculator ?: new TimeConsumptionCalculator;
+        $this->iCalculateLaserOrder = $iCalculateLaserOrder ?: new LaserOrder;
+
+        $this->authentication = $authentication;
+        $this->privilegesManagement = $privilegesManagement;
+    }
 }
