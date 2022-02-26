8a70c5757841af1dd39037c8350a7944e44e666e
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-26 01:02:27 -0800

    Add methods for retrieving laser orders in IDataBaseRetrieveLaserOrders

--- Src/Orders/Interfaces/IDataBaseRetrieveLaserOrders.php
+++ Src/Orders/Interfaces/IDataBaseRetrieveLaserOrders.php
@@ -8,7 +8,41 @@ use TheClinicDataStructures\DataStructures\User\DSUser;
 
 interface IDataBaseRetrieveLaserOrders
 {
+    /**
+     * @param string $operator Must be one the followings: "<=" ">=" "=" "<>" "<" ">"
+     * @param integer $price
+     * @param \TheClinicDataStructures\DataStructures\User\DSUser $targetUser
+     * @return \TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrders
+     */
+    public function getLaserOrdersByPriceByUser(string $operator, int $price, DSUser $targetUser): DSLaserOrders;
+
+    /**
+     * @param string $operator Must be one the followings: "<=" ">=" "=" "<>" "<" ">"
+     * @param integer $price
+     * @param \TheClinicDataStructures\DataStructures\User\DSUser $targetUser
+     * @return \TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrders
+     */
+    public function getLaserOrdersByPrice(int $lastOrderId = null, int $count, string $operator, int $price): DSLaserOrders;
+
+    /**
+     * @param string $operator Must be one the followings: "<=" ">=" "=" "<>" "<" ">"
+     * @param integer $price
+     * @param \TheClinicDataStructures\DataStructures\User\DSUser $targetUser
+     * @return \TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrders
+     */
+    public function getLaserOrdersByTimeConsumptionByUser(string $operator, int $timeCosumption, DSUser $targetUser): DSLaserOrders;
+
+    /**
+     * @param string $operator Must be one the followings: "<=" ">=" "=" "<>" "<" ">"
+     * @param integer $price
+     * @param \TheClinicDataStructures\DataStructures\User\DSUser $targetUser
+     * @return \TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrders
+     */
+    public function getLaserOrdersByTimeConsumption(int $lastOrderId = null, int $count, string $operator, int $timeCosumption): DSLaserOrders;
+
+    public function getLaserOrdersByUser(DSUser $targetUser): DSLaserOrders;
+
     public function getLaserOrders(int $lastOrderId = null, int $count): DSLaserOrders;
-    
-    public function getLaserOrder(DSUser $targetUser): DSLaserOrder;
+
+    public function getLaserOrderById(int $id): DSLaserOrder;
 }
