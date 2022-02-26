2f4d31ca060c8edc13a6e33f44c3ddcf428b6c88
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-25 20:10:29 -0800

    Add a method for getting one laser order in IDataBaseRetrieveLaserOrders

--- Src/Orders/Interfaces/IDataBaseRetrieveLaserOrders.php
+++ Src/Orders/Interfaces/IDataBaseRetrieveLaserOrders.php
@@ -2,9 +2,13 @@
 
 namespace TheClinicUseCases\Orders\Interfaces;
 
+use TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrder;
 use TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrders;
+use TheClinicDataStructures\DataStructures\User\DSUser;
 
 interface IDataBaseRetrieveLaserOrders
 {
     public function getLaserOrders(int $lastOrderId = null, int $count): DSLaserOrders;
+    
+    public function getLaserOrder(DSUser $targetUser): DSLaserOrder;
 }
