61d6219f05c4110ab2a6c6bfb1ea786a057ba8dc
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-27 04:39:34 -0800

    Update PrivilegesManagementTest test class

--- Tests/Privileges/PrivilegesManagementTest.php
+++ Tests/Privileges/PrivilegesManagementTest.php
@@ -5,9 +5,9 @@ namespace Tests\Privileges;
 use Faker\Factory;
 use Faker\Generator;
 use Mockery;
+use Mockery\MockInterface;
 use Tests\TestCase;
 use TheClinicDataStructures\DataStructures\User\DSUser;
-use TheClinicDataStructures\DataStructures\User\IUserRule;
 use TheClinicUseCases\Exceptions\Accounts\UserIsNotAuthorized;
 use TheClinicUseCases\Exceptions\PrivilegeNotFound;
 use TheClinicUseCases\Privileges\PrivilegesManagement;
@@ -16,55 +16,80 @@ class PrivilegesManagementTest extends TestCase
 {
     private Generator $faker;
 
+    private DSUser|MockInterface $user;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->faker = Factory::create();
+
+        /** @var \TheClinicDataStructures\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+        $this->user = Mockery::mock(DSUser::class);
+    }
+
+    public function testGetUserPrivileges(): void
+    {
+        $this->user->shouldReceive("getUserPrivileges")->andReturn([]);
+
+        $privileges = (new PrivilegesManagement)->getUserPrivileges($this->user);
+
+        $this->assertIsArray($privileges);
+        $this->assertCount(0, $privileges);
+    }
+
+    public function testGetUserPrivilege(): void
+    {
+        $privilege = "privilege";
+
+        $this->user->shouldReceive("getPrivilege")->with($privilege)->andReturn(true);
+
+        $privilege = (new PrivilegesManagement)->getUserPrivilege($this->user, $privilege);
+
+        $this->assertIsBool($privilege);
+        $this->assertEquals(true, $privilege);
+    }
+
+    public function testSetUserPrivilege(): void
+    {
+        $privilege = "privilege";
+        $value = true;
+
+        $this->user->shouldReceive("setPrivilege")->with($privilege, $value)->andReturn(null);
+
+        $result = (new PrivilegesManagement)->setUserPrivilege($this->user, $privilege, $value);
+
+        $this->assertNull($result);
     }
 
     public function testCheckBool(): void
     {
         $privilege = "ExampleBooleanPrivilege";
 
-        /** @var \TheClinicDataStructures\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
-        $rule = Mockery::mock(IUserRule::class);
-        $rule->shouldReceive("privilegeExists")->with($privilege)->andReturn(true);
-        $rule->shouldReceive("getPrivilegeValue")->with($privilege)->andReturn(true);
+        $this->user->shouldReceive("privilegeExists")->with($privilege)->andReturn(true);
+        $this->user->shouldReceive("getPrivilege")->with($privilege)->andReturn(true);
 
-        /** @var \TheClinicDataStructures\DataStructures\User\DSUser|\Mockery\MockInterface $dsUser */
-        $dsUser = Mockery::mock(DSUser::class);
-        $dsUser->shouldReceive("getRule")->andReturn($rule);
-
-        $result = (new PrivilegesManagement)->checkBool($dsUser, $privilege);
+        $result = (new PrivilegesManagement)->checkBool($this->user, $privilege);
         $this->assertNull($result);
 
         try {
-            /** @var \TheClinicDataStructures\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
-            $rule = Mockery::mock(IUserRule::class);
-            $rule->shouldReceive("privilegeExists")->with($privilege)->andReturn(false);
-            $rule->shouldReceive("getPrivilegeValue")->with($privilege)->andReturn(true);
+            /** @var \TheClinicDataStructures\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+            $this->user = Mockery::mock(DSUser::class);
+            $this->user->shouldReceive("privilegeExists")->with($privilege)->andReturn(false);
+            $this->user->shouldReceive("getPrivilege")->with($privilege)->andReturn(true);
 
-            /** @var \TheClinicDataStructures\DataStructures\User\DSUser|\Mockery\MockInterface $dsUser */
-            $dsUser = Mockery::mock(DSUser::class);
-            $dsUser->shouldReceive("getRule")->andReturn($rule);
-
-            $result = (new PrivilegesManagement)->checkBool($dsUser, $privilege);
+            $result = (new PrivilegesManagement)->checkBool($this->user, $privilege);
             throw new \RuntimeException("Failure!!!", 500);
         } catch (PrivilegeNotFound $th) {
         }
 
         try {
-            /** @var \TheClinicDataStructures\DataStructures\User\IUserRule|\Mockery\MockInterface $rule */
-            $rule = Mockery::mock(IUserRule::class);
-            $rule->shouldReceive("privilegeExists")->with($privilege)->andReturn(true);
-            $rule->shouldReceive("getPrivilegeValue")->with($privilege)->andReturn(false);
+            /** @var \TheClinicDataStructures\DataStructures\User\DSUser|\Mockery\MockInterface $user */
+            $this->user = Mockery::mock(DSUser::class);
+            $this->user->shouldReceive("privilegeExists")->with($privilege)->andReturn(true);
+            $this->user->shouldReceive("getPrivilege")->with($privilege)->andReturn(false);
 
-            /** @var \TheClinicDataStructures\DataStructures\User\DSUser|\Mockery\MockInterface $dsUser */
-            $dsUser = Mockery::mock(DSUser::class);
-            $dsUser->shouldReceive("getRule")->andReturn($rule);
-
-            $result = (new PrivilegesManagement)->checkBool($dsUser, $privilege);
+            $result = (new PrivilegesManagement)->checkBool($this->user, $privilege);
             throw new \RuntimeException("Failure!!!", 500);
         } catch (UserIsNotAuthorized $th) {
         }
