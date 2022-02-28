38d5e01140b4ed0676e6d33fdfa6ebbcd99ef2d7
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-27 22:52:16 -0800

    Fix IDataBaseCreateVisit interface createLaserVisit method

--- Src/Visits/Interfaces/IDataBaseCreateVisit.php
+++ Src/Visits/Interfaces/IDataBaseCreateVisit.php
@@ -2,12 +2,14 @@
 
 namespace TheClinicUseCases\Visits\Interfaces;
 
+use TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrder;
 use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrder;
+use TheClinicDataStructures\DataStructures\Visit\Laser\DSLaserVisit;
 use TheClinicDataStructures\DataStructures\Visit\Regular\DSRegularVisit;
 
 interface IDataBaseCreateVisit
 {
     public function createRegularVisit(DSRegularOrder $dsRegularOrder, int $timestamp): DSRegularVisit;
 
-    public function createLaserVisit(DSRegularOrder $dsRegularOrder, int $timestamp): DSRegularVisit;
+    public function createLaserVisit(DSLaserOrder $dsLaserOrder, int $timestamp): DSLaserVisit;
 }
