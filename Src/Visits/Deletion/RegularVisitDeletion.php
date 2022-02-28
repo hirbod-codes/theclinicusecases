5f81ecf0ac9dc8f599851226c600bebc33469b06
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-27 23:20:06 -0800

    Add delete method in RegularVisitDeletion class

--- Src/Visits/Deletion/RegularVisitDeletion.php
+++ Src/Visits/Deletion/RegularVisitDeletion.php
@@ -2,6 +2,37 @@
 
 namespace TheClinicUseCases\Visits\Deletion;
 
+use TheClinicDataStructures\DataStructures\User\DSUser;
+use TheClinicDataStructures\DataStructures\Visit\Regular\DSRegularVisit;
+use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Privileges\PrivilegesManagement;
+use TheClinicUseCases\Visits\Interfaces\IDataBaseDeleteVisit;
+
 class RegularVisitDeletion
 {
+    private Authentication $authentication;
+
+    private PrivilegesManagement $privilegesManagement;
+
+    public function __construct(
+        Authentication $authentication,
+        PrivilegesManagement $privilegesManagement
+    ) {
+        $this->authentication = $authentication;
+        $this->privilegesManagement = $privilegesManagement;
+    }
+
+    public function delete(DSRegularVisit $dsRegularVisit, DSUser $user, IDataBaseDeleteVisit $db): void
+    {
+        if ($dsRegularVisit->getOrder()->getUser()->getId() === $user->getId()) {
+            $privilege = "selfRegularVisitDelete";
+        } else {
+            $privilege = "regularVisitDelete";
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, $privilege);
+
+        $db->deleteRegularVisit($dsRegularVisit);
+    }
 }
