9cb5b5efc9a99e5aaf9331af62e2483494a24810
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-27 23:23:28 -0800

    Add delete method in LaserVisitDeletion class

--- Src/Visits/Deletion/LaserVisitDeletion.php
+++ Src/Visits/Deletion/LaserVisitDeletion.php
@@ -2,6 +2,37 @@
 
 namespace TheClinicUseCases\Visits\Deletion;
 
+use TheClinicDataStructures\DataStructures\User\DSUser;
+use TheClinicDataStructures\DataStructures\Visit\Laser\DSLaserVisit;
+use TheClinicUseCases\Accounts\Authentication;
+use TheClinicUseCases\Privileges\PrivilegesManagement;
+use TheClinicUseCases\Visits\Interfaces\IDataBaseDeleteVisit;
+
 class LaserVisitDeletion
 {
+    private Authentication $authentication;
+
+    private PrivilegesManagement $privilegesManagement;
+
+    public function __construct(
+        Authentication $authentication,
+        PrivilegesManagement $privilegesManagement
+    ) {
+        $this->authentication = $authentication;
+        $this->privilegesManagement = $privilegesManagement;
+    }
+
+    public function delete(DSLaserVisit $dsLaserVisit, DSUser $user, IDataBaseDeleteVisit $db): void
+    {
+        if ($dsLaserVisit->getOrder()->getUser()->getId() === $user->getId()) {
+            $privilege = "selfLaserVisitDelete";
+        } else {
+            $privilege = "laserVisitDelete";
+        }
+
+        $this->authentication->check($user);
+        $this->privilegesManagement->checkBool($user, $privilege);
+
+        $db->deleteLaserVisit($dsLaserVisit);
+    }
 }
