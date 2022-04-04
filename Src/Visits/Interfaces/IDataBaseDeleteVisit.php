cae99053ba7cc3ff20b78ad7212d77ab7c1ca671
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-04 05:03:52 -0700

    Update IDataBaseDeleteVisit interface

--- Src/Visits/Interfaces/IDataBaseDeleteVisit.php
+++ Src/Visits/Interfaces/IDataBaseDeleteVisit.php
@@ -2,12 +2,13 @@
 
 namespace TheClinicUseCases\Visits\Interfaces;
 
+use TheClinicDataStructures\DataStructures\User\DSUser;
 use TheClinicDataStructures\DataStructures\Visit\Laser\DSLaserVisit;
 use TheClinicDataStructures\DataStructures\Visit\Regular\DSRegularVisit;
 
 interface IDataBaseDeleteVisit
 {
-    public function deleteRegularVisit(DSRegularVisit $dsRegularVisit): void;
+    public function deleteRegularVisit(DSRegularVisit $dsRegularVisit, DSUser $targetUser): void;
 
-    public function deleteLaserVisit(DSLaserVisit $dsLaserVisit): void;
+    public function deleteLaserVisit(DSLaserVisit $dsLaserVisit, DSUser $targetUser): void;
 }
