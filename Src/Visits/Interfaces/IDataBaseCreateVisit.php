250c4fd6a55f57ae13d7fbd9007ab25a5d0b3c7d
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-04 05:03:35 -0700

    Update IDataBaseCreateVisit interface

--- Src/Visits/Interfaces/IDataBaseCreateVisit.php
+++ Src/Visits/Interfaces/IDataBaseCreateVisit.php
@@ -4,12 +4,13 @@ namespace TheClinicUseCases\Visits\Interfaces;
 
 use TheClinicDataStructures\DataStructures\Order\Laser\DSLaserOrder;
 use TheClinicDataStructures\DataStructures\Order\Regular\DSRegularOrder;
+use TheClinicDataStructures\DataStructures\User\DSUser;
 use TheClinicDataStructures\DataStructures\Visit\Laser\DSLaserVisit;
 use TheClinicDataStructures\DataStructures\Visit\Regular\DSRegularVisit;
 
 interface IDataBaseCreateVisit
 {
-    public function createRegularVisit(DSRegularOrder $dsRegularOrder, int $timestamp): DSRegularVisit;
+    public function createRegularVisit(DSRegularOrder $dsRegularOrder, DSUser $targetUser, int $timestamp): DSRegularVisit;
 
-    public function createLaserVisit(DSLaserOrder $dsLaserOrder, int $timestamp): DSLaserVisit;
+    public function createLaserVisit(DSLaserOrder $dsLaserOrder, DSUser $targetUser, int $timestamp): DSLaserVisit;
 }
