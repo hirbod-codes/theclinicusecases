af4c2569513a7fa9b507bfd1211492da38bebd5f
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-13 03:09:17 -0700

    Move $iFindVisit Parameter fomr __construct to create method in RegularVisitCreation

--- Src/Visits/Creation/RegularVisitCreation.php
+++ Src/Visits/Creation/RegularVisitCreation.php
@@ -20,26 +20,21 @@ class RegularVisitCreation
 
     private PrivilegesManagement $privilegesManagement;
 
-    private IFindVisit $iFindVisit;
-
     public function __construct(
-        IFindVisit $iFindVisit,
         Authentication $authentication = null,
         PrivilegesManagement $privilegesManagement = null
     ) {
         $this->authentication = $authentication ?: new Authentication;
         $this->privilegesManagement = $privilegesManagement ?: new PrivilegesManagement;
-
-        $this->iFindVisit = $iFindVisit;
     }
 
-    public function create(DSRegularOrder $dsRegularOrder, DSUser $targetUser, DSUser $user, IDataBaseCreateRegularVisit $db): DSRegularVisit
+    public function create(DSRegularOrder $dsRegularOrder, DSUser $targetUser, DSUser $user, IDataBaseCreateRegularVisit $db, IFindVisit $iFindVisit): DSRegularVisit
     {
         $privilege = $this->getPrivilegeFromInput($user, $targetUser, "selfRegularVisitCreate", "regularVisitCreate");
 
         $this->authentication->check($user);
         $this->privilegesManagement->checkBool($user, $privilege);
 
-        return $db->createRegularVisit($dsRegularOrder, $targetUser, $this->iFindVisit);
+        return $db->createRegularVisit($dsRegularOrder, $targetUser, $iFindVisit);
     }
 }
